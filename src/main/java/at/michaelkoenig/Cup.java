774195990bc46d36e2745fb2cbe89f6ce652881a
Michael König <koenigmichael2002@gmail.com>
2020-03-19 08:39:13 +0100

    Add author annotations

--- src/main/java/at/michaelkoenig/Cup.java
+++ src/main/java/at/michaelkoenig/Cup.java
@@ -1,10 +1,8 @@
 package at.michaelkoenig;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+/**
+ * @author Michael König
+ */
 public class Cup {
 
     private String liquidType;
--- src/test/java/at/michaelkoenig/CupTest.java
+++ src/test/java/at/michaelkoenig/CupTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @author Michael König
+ */
 class CupTest {
 
     @Test
